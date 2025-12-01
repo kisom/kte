e869249a7cd3b90847211c79e51983cbabf83ba5
Kyle Isom <kyle@imap.cc>
2025-11-30 22:02:23 -0800

    thaaaaanks jeremy

--- default.nix
+++ default.nix
@@ -7,12 +7,16 @@
   libGL,
   xorg,
   installShellFiles,
+
+  graphical ? true,
   ...
 }:
 let
   cmakeContent = builtins.readFile ./CMakeLists.txt;
   cmakeLines = lib.splitString "\n" cmakeContent;
-  versionLine = lib.findFirst (l: builtins.match ".*set\\(KTE_VERSION \".+\"\\).*" l != null) (throw "KTE_VERSION not found in CMakeLists.txt") cmakeLines;
+  versionLine = lib.findFirst (
+    l: builtins.match ".*set\\(KTE_VERSION \".+\"\\).*" l != null
+  ) (throw "KTE_VERSION not found in CMakeLists.txt") cmakeLines;
   version = builtins.head (builtins.match ".*set\\(KTE_VERSION \"(.+)\"\\).*" versionLine);
 in
 stdenv.mkDerivation {
@@ -24,14 +28,16 @@ stdenv.mkDerivation {
   nativeBuildInputs = [
     cmake
     ncurses
+    installShellFiles
+  ]
+  ++ lib.optionals graphical [
     SDL2
     libGL
     xorg.libX11
-    installShellFiles
   ];
 
   cmakeFlags = [
-    "-DBUILD_GUI=ON"
+    "-DBUILD_GUI=${if graphical then "ON" else "OFF"}"
     "-DCMAKE_BUILD_TYPE=Debug"
   ];
 
@@ -40,14 +46,17 @@ stdenv.mkDerivation {
 
     mkdir -p $out/bin
     cp kte $out/bin/
-    cp kge $out/bin/
 
     installManPage ../docs/kte.1
-    installManPage ../docs/kge.1
 
+  ''
+  + lib.optionalString graphical ''
+    cp kge $out/bin/
+    installManPage ../docs/kge.1
     mkdir -p $out/share/icons
     cp ../kge.png $out/share/icons/
-
+  ''
+  + ''
     runHook postInstall
   '';
 }
--- flake.nix
+++ flake.nix
@@ -3,16 +3,18 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = inputs @ { self, nixpkgs, ... }:
+  outputs =
+    inputs@{ self, nixpkgs, ... }:
     let
       eachSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
       pkgsFor = system: import nixpkgs { inherit system; };
-    in {
-      packages = eachSystem (system: {
-        default = (pkgsFor system).callPackage ./default-nogui.nix { };
-        kge     = (pkgsFor system).callPackage ./default-gui.nix   { };
-        kte     = (pkgsFor system).callPackage ./default-nogui.nix { };
-        full    = (pkgsFor system).callPackage ./default.nix       { };
+    in
+    {
+      packages = eachSystem (system: rec {
+        default = kte;
+        full = kge;
+        kte = (pkgsFor system).callPackage ./default.nix { graphical = false; };
+        kge = (pkgsFor system).callPackage ./default.nix { graphical = true; };
       });
     };
 }
